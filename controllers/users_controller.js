272bcaf692417080bb66280fed99e44b6a03cc93
Chetankumar19 <you@example.com>
2022-03-10 02:42:12 +0530

    make two page sign_in or sign_out

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,24 @@
 module.exports.profile = function(req,res){
     return res.end('<h1>User Profile</h1>')
 };
+//render the signUp page
+module.exports.signUp = function(req,res){
+    return res.render('user_sign_up',{
+        title:"Codeial | Sign Up"
+    })
+};
+// render the signIn page
+module.exports.signIn = function(req,res){
+    return res.render('user_sign_in',{
+        title:"Codeial | Sign In"
+    })
+};
+
+//get the sign up data
+module.exports.create =  function(req,res){
+    // Todo later
+}
+//sign in and create a session for the user 
+module.exports.createSession = function(req,res){
+    // todo later
+}
