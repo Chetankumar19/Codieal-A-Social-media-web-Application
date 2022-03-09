06342fa73ddae8147c5e185d1ff24b51cbb624a3
Chetankumar19 <you@example.com>
2022-03-10 02:11:47 +0530

    just update

--- index.js
+++ index.js
@@ -3,8 +3,11 @@ const app = express();
 
 const port = 8000;
 
-app.use('/',require('./routes/index'));
+app.use('/',require('./routes'));
 
+// set the view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 app.listen(port,function(err){
     if(err){
         console.log('Error in:');
--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,12 @@ const express  = require("express");
 const router = express.Router();
 
 const homeController = require('../controllers/home_controller')
+console.log('router loaded');
 
 router.get('/',homeController.home);
+
+router.use('/users',require('./users'));
+//for any futer routes , acces from here
+// router.use('/routerName',requie('./routerfile'));
 
 module.exports = router;
